a756d124c0c1871b3410095569d5789b79f9b9b7
Nanda verdeny <verdenynanda@gmail.com>
2017-06-02 11:03:27 +0700

    Initial commit v.1.0.0

---
+++ src/NETCore_AuthFramework_PostgresSQL/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETCore_AuthFramework_PostgresSQL.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
